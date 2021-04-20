0a82b51989d612f9f5267379165568e2ef752cb8
Vladislav Vazhko <vazhko@gmail.com>
2021-04-20 12:19:08 +0300

    001 twoSumTest

--- app/src/androidTest/java/com/example/leetcode_tasks/SolutionsTest.java
+++ app/src/androidTest/java/com/example/leetcode_tasks/SolutionsTest.java
@@ -1,22 +1,13 @@
 package com.example.leetcode_tasks;
 
+import junit.framework.TestCase;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class SolutionsUnitTest {
-
-
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+public class SolutionsTest  {
 
     Solutions s;
 
@@ -29,6 +20,7 @@ public class SolutionsUnitTest {
     public void twoSumTest() {
 
         int arr[] = {1,2,3,4,5,9,5};
+        //int arr[] = {6, 1, 0};
         int res = 6;
         int sol[] = s.twoSum(arr, res);
 
--- app/src/main/java/com/example/leetcode_tasks/Solutions.java
+++ app/src/main/java/com/example/leetcode_tasks/Solutions.java
@@ -1,50 +1,60 @@
 package com.example.leetcode_tasks;
 
+import android.util.Log;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solutions {
 
- /** 1. Two Sum
-Easy
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
+    /**
+     * 1. Two Sum
+     * Easy
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * You can return the answer in any order.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+     * Example 2:
+     * <p>
+     * Input: nums = [3,2,4], target = 6
+     * Output: [1,2]
+     * Example 3:
+     * <p>
+     * Input: nums = [3,3], target = 6
+     * Output: [0,1]
+     * <p>
+     * Constraints:
+     * 2 <= nums.length <= 103
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * Only one valid answer exists.
+     */
 
-Example 1:
+    public int[] twoSum(int[] nums, int target) {
+        int c1 = 0, c2 = 0;
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
+        Map<Integer, Integer> map = new HashMap<>(20);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if ((map.containsKey(complement) && map.get(complement) != i)) {
+                Log.i("Solutions", "sol: {" + i +", " + map.get(complement) + "}");
+                return new int[]{i, map.get(complement)};
+            }
+        }
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
+        throw new IllegalArgumentException("No two sum solution");
 
-Constraints:
-2 <= nums.length <= 103
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
-  */
-
- public int[] twoSum(int[] nums, int target) {
-    int c1 = 0, c2 = 0;
-
-     Map<Integer, Integer> map = new HashMap<>();
-     for (int i = 0; i < nums.length; i++) {
-         map.put(nums[i], i);
-     }
+    }
 
 
-
-    return new int[] { c1, c2 };
- }
-
-    
 }
 
